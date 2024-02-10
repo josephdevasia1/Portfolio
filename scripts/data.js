974bdebcf248ed91d30d494c1882a24ddccd7b76
Joseph Devasia <56888247+josephdevasia1@users.noreply.github.com>
2024-02-10 06:56:09 +0530

    initial commit

---
+++ scripts/data.js
@@ -0,0 +1,77 @@
+const linkedin = "https://www.linkedin.com/in/joseph-devasia-881a541a9/";
+const github = "https://github.com/josephdevasia1/";
+const email = "mailto:joseph.devasia1@outlook.com";
+var today = new Date();
+
+about = [
+  "<br>",
+  "Hey, I'm Joseph Devasia! 👋",
+  "<br>",
+  "I'm a Computer Science Engineer,.",
+  "I'm also a freelance full stack dev.'",
+  "Having said that, I'm always ready to work on quirky projects",
+  "I am always ready to explore new things and collaborate with others.",
+  "Gathering a lot of knowledge and insights.",
+  "Outside of the professional space, I love playing football and hangout with my homies",
+  "<br>",
+  "Hope you explore and enjoy this website! 🙏🏻",
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  '<span class = "index">' + String(today) + "</span>",
+  '<span class="index">Joseph Devasia </span>',
+  "<br>",
+
+  "<br>",
+  '<span class="color2">Welcome to my Portfolio Website..</span>',
+  '<span class="color2">For a list of available commands, type</span> <span class="command">\'help\'</span><span class="color2">.</span>',
+  // '<span class="color2"></span>',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '👦🏻  <span class="command">About</span>          About Me',
+  '🌏  <span class="command">Social</span>         Display social networks',
+  '🏗  <span class="command">Projects</span>       View coding projects',
+  '📧  <span class="command">Email</span>          Reach me through email',
+  '🧹  <span class="command">Clear</span>          Clear the terminal',
+  '❓  <span class="command">Help</span>           Open the help menu',
+  '🕰  <span class="command">History</span>        View command history',
+  '👋  <span class="command">Exit</span>           Close the tab and exit',
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "Enter the name of any of the following or simply click the links...",
+  "<br>",
+  'Linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/joseph-devasia-881a541a9' +
+    "</a>",
+  'Github         <a href="' +
+    github +
+    '" target="_blank">github/josephdevasia1' +
+    "</a>",
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  '<span class="command">1. WeatherPedia</span>',
+  "<br>",
+  "<br>",
+  '<span class="command">2. Mental Health Fitness Tracker',
+  "<br>",
+  "<br>",
+  '<span class="command">3. Portfolio Website',
+  "<br>",
+  "<br>",
+  '<span class="command">4.Portfolio Website (This Website)</span>',
+  "<br>",
+  'Click the following links to access all my projects: <a href = "https://github.com/josehdevasia1/" target="_blank">(Github)</a>',
+  "<br>",
+];
